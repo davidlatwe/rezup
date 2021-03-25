080dea9d38be4a6c9dd0f48c0cf7272606f4b2b9
davidlatwe <davidlatwe@gmail.com>
2021-03-26 05:34:23 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # rezup
+
+Rez launching environment manager
+
+### Usage
+
+use default, if container default not exists, get rez from pypi
+```
+$ rezup
+```
+
+use foo
+```
+$ rezup --use foo
+```
+
+use foo and do job
+```
+$ rezup --use foo --do job
+```
+
+use default to do job
+```
+$ rezup --do job
+```
+
+install rez into container default
+```
+$ rezup add
+```
+
+install rez into container foo
+```
+$ rezup add --name foo
+```
+
+install rez into container live, which will be in edit mode
+```
+$ rezup add --name live
+```
+
+remove container foo
+```
+$ rezup drop --name foo
+```
+
+list containers
+```
+$ rezup list
+```
--- src/rezup/cli.py
+++ src/rezup/cli.py
@@ -60,35 +60,6 @@ def get_shell():
 
 
 def run():
-    """
-    use default, if container default not exists, get rez from pypi
-    $ rezup
-
-    use foo
-    $ rezup --use foo
-
-    use foo and do job
-    $ rezup --use foo --do job
-
-    use default to do job
-    $ rezup --do job
-
-    install rez into container default
-    $ rezup add
-
-    install rez into container foo
-    $ rezup add --name foo
-
-    install rez into container live, which will be in edit mode
-    $ rezup add --name live
-
-    remove container foo
-    $ rezup drop --name foo
-
-    list containers
-    $ rezup list
-
-    """
     parser = argparse.ArgumentParser("rezup")
 
     parser.add_argument("-u", "--use", default=None)
