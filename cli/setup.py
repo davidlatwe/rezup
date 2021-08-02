31bdab394d1dbe93df58152f6ff08f2a1531dc9a
David Lai <davidlatwe@gmail.com>
2021-08-03 00:57:28 +0800

    fix rezup cli setup.py

--- cli/setup.py
+++ cli/setup.py
@@ -1,13 +1,3 @@
 
 from setuptools import setup
-
-
-g = {"__version__": ""}
-with open("../src/rezup/_version.py") as f:
-    exec(f.read(), g)
-__version__ = g["__version__"]
-
-setup(
-    version=__version__,
-    install_requires=["rezup-api==" + __version__]
-)
+setup()
