cd904a487c9b9b4090c1a16325173322c735dfaf
davidlatwe <davidlatwe@gmail.com>
2021-10-17 00:26:19 +0800

    improve remote container pulling test

--- tests/test_container.py
+++ tests/test_container.py
@@ -37,24 +37,49 @@ class TestContainer(TestBase):
         con_name = "foo"
         self.setup_remote()
 
+        # creates a revision 'apple' to remote side
+        self.save_recipe(con_name, data={"description": "apple"})
         container = Container.create(con_name)
-        self.assertTrue(container.is_remote())
-
-        self.save_recipe(con_name)
         revision = container.new_revision()
-
-        self.assertTrue(revision.is_valid())
-        self.assertTrue(revision.is_ready())
+        self.assertTrue(container.is_remote())
         self.assertTrue(revision.is_remote())
+        self.assertTrue(revision.is_ready())
+        self.assertEqual("apple", container.recipe()["description"])
+        self.assertEqual("apple", revision.recipe()["description"])
 
-        pulled_rev = revision.pull()
-
+        # shouldn't have any local revision yet
         local = Container.create(con_name, force_local=True)
-        self.assertFalse(local.is_remote())
-
         local_rev = local.get_latest_revision()
+        local_count = len(list(local.iter_revision()))
+        self.assertEqual(False, local.is_remote())
+        self.assertEqual(None, local_rev)
+        self.assertEqual(0, local_count)
 
-        self.assertEqual(pulled_rev.timestamp(), local_rev.timestamp())
+        # change container recipe to 'orange'
+        self.save_recipe(con_name, data={"description": "orange"})
+
+        # in new session, pull revision from remote and it should be 'apple'
+        container = Container.create(con_name)
+        revision = container.get_latest_revision()
+        pulled_rev = revision.pull()
+        self.assertTrue(container.is_remote())
+        self.assertEqual(True, revision.is_remote())
+        self.assertEqual(False, pulled_rev.is_remote())
+        self.assertTrue(revision.is_ready())
+        self.assertTrue(pulled_rev.is_ready())
+        self.assertEqual("orange", container.recipe()["description"])
+        self.assertEqual("apple", revision.recipe()["description"])
+        self.assertEqual("apple", pulled_rev.recipe()["description"])
+
+        # now we should have 1 revision 'apple' at local
+        local = Container.create(con_name, force_local=True)
+        local_rev = local.get_latest_revision()
+        local_count = len(list(local.iter_revision()))
+        self.assertEqual(False, local.is_remote())
+        self.assertEqual(1, local_count)
+        self.assertEqual(local_rev.timestamp(), pulled_rev.timestamp())
+        self.assertEqual("orange", local.recipe()["description"])
+        self.assertEqual("apple", local_rev.recipe()["description"])
 
     def test_recipe_env(self):
         con_name = "foo"
