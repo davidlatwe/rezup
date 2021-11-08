3561eaa2ae8a81da128ded88e0ce40dc71ee5e1d
davidlatwe <davidlatwe@gmail.com>
2021-11-09 07:58:27 +0800

    update docstring

--- src/rezup/recipe.py
+++ src/rezup/recipe.py
@@ -99,7 +99,7 @@ class ContainerRecipe(BaseRecipe):
         * `~/rezup.test.toml` -> container `test`
 
     Args:
-        name (str): Container name
+        name (str, optional): Container name
 
     """
     REGEX = re.compile("rezup.?(.*).toml")
@@ -128,7 +128,7 @@ class ContainerRecipe(BaseRecipe):
         ```
 
         Args:
-            path (`str` or path-like): directory path where recipes located
+            path (str or path-like): directory path where recipes located
         """
         default = cls.RECIPES_DIR
         try:
@@ -154,7 +154,7 @@ class ContainerRecipe(BaseRecipe):
     def path(self):
         """Returns the file path of this recipe
         Returns:
-            `pathlib.Path`
+            pathlib.Path: The file path of this recipe
         """
         if self._path is None:
             self._path = self.RECIPES_DIR / self._file
@@ -163,9 +163,7 @@ class ContainerRecipe(BaseRecipe):
     def create(self, data=None):
         """Write out recipe content into a .toml file
         Args:
-            data (`dict`, optional): Arbitrary data to write out
-        Returns:
-            `None`
+            data (dict, optional): Arbitrary data to write out
         """
         if not self.RECIPES_DIR.is_dir():
             self.RECIPES_DIR.mkdir(parents=True)
@@ -187,7 +185,7 @@ class ContainerRecipe(BaseRecipe):
 class RevisionRecipe(BaseRecipe):
     """A dict-like representation of revision's internal recipe
     Args:
-        revision(Revision): A rezup revision instance
+        revision (Revision): A rezup revision instance
     """
     def __init__(self, revision):
         name = revision.container().name()
